de7e8c69a7c5484ab51c4e139d44eb29122bd9b2
JSH32 <rudnik7000@gmail.com>
2023-01-02 15:46:35 -0600

    Fix binary body type

--- ephermal-worker/src/extensions/http.rs
+++ ephermal-worker/src/extensions/http.rs
@@ -1,5 +1,9 @@
 use crate::runtime::extension::{ExtensionInfo, LuaExtension};
-use hyper::{header::HeaderName, http, Body, Client, Version};
+use hyper::{
+    header::HeaderName,
+    http::{self, uri::InvalidUri},
+    Client, Version,
+};
 use hyper_tls::HttpsConnector;
 use mlua::{ExternalResult, LuaSerdeExt, UserData};
 use serde::{Deserialize, Serialize};
@@ -23,13 +27,20 @@ impl LuaExtension for HttpExtension {
         let http = lua.create_table()?;
         http.set(
             "make_request",
-            lua.create_async_function(|lua, request: mlua::Value| async move {
-                let lua_request: Request = lua.from_value(request)?;
+            lua.create_async_function(|lua, request: mlua::Table| async move {
+                // Since we accept userdata, we need to do this hack to allow for conversion.
+                let lua_request: Request =
+                    serde_json::from_str(&serde_json::to_string(&request).to_lua_err()?)
+                        .to_lua_err()?;
+
                 let request: mlua::Result<hyper::Request<_>> = lua_request.clone().into();
+
+                let hyper_uri: http::Result<hyper::Uri> =
+                    lua_request.uri.to_uri().to_lua_err()?.into();
 
                 debug!(
                     request = ?lua_request,
-                    "Making request to {}", lua_request.uri
+                    "Making request to {}", hyper_uri.to_lua_err()?.to_string()
                 );
 
                 let response = Response::new(
@@ -45,25 +56,77 @@ impl LuaExtension for HttpExtension {
             })?,
         )?;
 
+        let uri_class = lua.create_proxy::<Uri>()?;
+        http.set("Uri", uri_class.clone())?;
+        lua.globals().set("Uri", uri_class)?;
+
+        // let body_class = lua.create_proxy::<Body>()?;
+        // http.set("Body", body_class.clone())?;
+        // lua.globals().set("Body", body_class)?;
+
         Ok(mlua::Value::Table(http))
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(untagged)]
+enum UriType {
+    Uri(Uri),
+    String(String),
+}
+
+impl UriType {
+    pub fn to_uri(&self) -> Result<Uri, InvalidUri> {
+        Ok(match self {
+            UriType::Uri(uri) => uri.clone(),
+            UriType::String(v) => Uri::from(hyper::Uri::from_str(&v)?),
+        })
     }
 }
 
 /// Lua userland request type.
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Request {
-    uri: String,
+    uri: UriType,
     method: String,
     #[serde(default)]
     headers: HashMap<String, String>,
     version: Option<String>,
-    body: Option<String>,
+    body: Option<BodyType>,
+}
+#[derive(Serialize, Deserialize, Clone, Debug)]
+#[serde(untagged)]
+
+pub enum BodyType {
+    Binary(Vec<u8>),
+    Text(String),
+}
+
+impl BodyType {
+    async fn from_hyper(body: hyper::Body) -> mlua::Result<Self> {
+        let body_bytes = hyper::body::to_bytes(body).await.to_lua_err()?;
+
+        Ok(match String::from_utf8(body_bytes.to_vec()) {
+            Ok(v) => BodyType::Text(v),
+            // Not a string
+            Err(_) => BodyType::Binary(body_bytes.to_vec()),
+        })
+    }
+}
+
+impl Into<hyper::Body> for BodyType {
+    fn into(self) -> hyper::Body {
+        match self {
+            BodyType::Binary(v) => hyper::Body::from(v),
+            BodyType::Text(v) => hyper::Body::from(v),
+        }
+    }
 }
 
 impl Request {
-    pub async fn new(request: hyper::Request<Body>) -> mlua::Result<Self> {
+    pub async fn new(request: hyper::Request<hyper::Body>) -> mlua::Result<Self> {
         Ok(Self {
-            uri: request.uri().to_string(),
+            uri: UriType::Uri(request.uri().clone().into()),
             method: request.method().to_string(),
             headers: {
                 request
@@ -73,27 +136,119 @@ impl Request {
                     .collect()
             },
             version: Some(format!("{:?}", request.version())),
-            body: Some(body_to_string(request.into_body()).await?),
+            body: Some(BodyType::from_hyper(request.into_body()).await?),
         })
     }
 }
 
 impl UserData for Request {}
 
-impl From<Request> for mlua::Result<hyper::Request<Body>> {
+impl From<Request> for mlua::Result<hyper::Request<hyper::Body>> {
     fn from(req: Request) -> Self {
+        let hyper_uri: http::Result<hyper::Uri> = req.uri.to_uri().to_lua_err()?.into();
+
         Ok(hyper::Request::builder()
-            .uri(req.uri)
+            .uri(hyper_uri.to_lua_err()?)
             .method(req.method.as_str())
             .version(match req.version {
                 Some(v) => string_to_version(&v)?,
                 None => Version::HTTP_11,
             })
             .body(match req.body {
-                Some(v) => Body::from(v),
-                None => Body::empty(),
+                Some(v) => v.into(),
+                None => hyper::Body::empty(),
             })
             .unwrap())
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Authority {
+    pub host: String,
+    pub port: Option<u16>,
+}
+
+impl UserData for Authority {}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Uri {
+    scheme: Option<String>,
+    authority: Option<Authority>,
+    path: String,
+    query: Option<String>,
+}
+
+impl UserData for Uri {
+    fn add_methods<'lua, M: mlua::UserDataMethods<'lua, Self>>(methods: &mut M) {
+        methods.add_method("tostring", |_, this, ()| {
+            let uri: http::Result<hyper::Uri> = this.to_owned().into();
+            Ok(uri.to_lua_err()?.to_string())
+        });
+
+        // Static constructors
+        methods.add_function("new", |lua, uri: mlua::Value| {
+            let lua_uri: Uri = lua.from_value(uri)?;
+            Ok(lua.create_ser_userdata(lua_uri)?)
+        });
+
+        methods.add_function("parse", |lua, uri: String| {
+            let uri = hyper::Uri::from_str(&uri).to_lua_err()?;
+            Ok(lua.create_ser_userdata(Uri::from(uri))?)
+        });
+    }
+
+    fn add_fields<'lua, F: mlua::UserDataFields<'lua, Self>>(fields: &mut F) {
+        fields.add_field_method_get("scheme", |_, this| Ok(this.scheme.clone()));
+        fields.add_field_method_get("authority", |_, this| Ok(this.authority.clone()));
+        fields.add_field_method_get("path", |_, this| Ok(this.path.clone()));
+        fields.add_field_method_get("query", |_, this| Ok(this.query.clone()));
+    }
+}
+
+impl From<hyper::Uri> for Uri {
+    fn from(uri: hyper::Uri) -> Self {
+        Self {
+            scheme: uri.scheme_str().map(str::to_string),
+            authority: match uri.authority() {
+                Some(v) => Some(Authority {
+                    host: v.host().to_string(),
+                    port: v.port_u16(),
+                }),
+                None => None,
+            },
+            path: uri.path().to_string(),
+            query: uri.query().map(str::to_string),
+        }
+    }
+}
+
+impl From<Uri> for http::Result<hyper::Uri> {
+    fn from(uri: Uri) -> Self {
+        let mut builder = hyper::Uri::builder().path_and_query(format!(
+            "{}{}",
+            uri.path,
+            match &uri.query {
+                Some(v) => v,
+                None => "",
+            }
+        ));
+
+        if let Some(scheme) = &uri.scheme {
+            builder = builder.scheme(scheme.as_str())
+        }
+
+        if let Some(authority) = &uri.authority {
+            builder = builder.authority(format!(
+                "{}{}",
+                authority.host,
+                match authority.port {
+                    Some(v) => format!(":{}", v),
+                    None => "".to_string(),
+                }
+            ));
+        }
+
+        builder.build()
     }
 }
 
@@ -102,7 +257,7 @@ impl From<Request> for mlua::Result<hyper::Request<Body>> {
 pub struct Response {
     pub status_code: Option<u16>,
     pub headers: Option<HashMap<String, String>>,
-    pub body: Option<String>,
+    pub body: Option<BodyType>,
 }
 
 impl UserData for Response {}
@@ -118,18 +273,22 @@ impl Response {
                     .map(|h| (h.0.to_string(), h.1.to_str().unwrap_or("").to_string()))
                     .collect(),
             ),
-            body: Some(body_to_string(response.into_body()).await?),
+            body: Some(
+                BodyType::from_hyper(response.into_body())
+                    .await
+                    .to_lua_err()?,
+            ),
         })
     }
 }
 
-impl From<Response> for http::Result<hyper::Response<Body>> {
+impl From<Response> for http::Result<hyper::Response<hyper::Body>> {
     fn from(res: Response) -> Self {
         // Build the response based on the returned json from lua.
         let mut response = hyper::Response::builder()
             .status(res.status_code.unwrap_or(200))
             .body(match res.body {
-                Some(v) => hyper::Body::from(v),
+                Some(v) => v.into(),
                 None => hyper::Body::empty(),
             })?;
 
@@ -163,9 +322,4 @@ fn string_to_version(str: &str) -> mlua::Result<Version> {
             )))
         }
     })
-}
-
-async fn body_to_string(body: hyper::Body) -> mlua::Result<String> {
-    let body_bytes = hyper::body::to_bytes(body).await.to_lua_err()?;
-    Ok(String::from_utf8(body_bytes.to_vec()).to_lua_err()?)
 }
--- ephermal-worker/src/test.lua
+++ ephermal-worker/src/test.lua
@@ -1,8 +1,15 @@
 add_event_listener("fetch", function(request)
     local response = http.make_request({
-        uri = "https://api.waifu.pics/nsfw/waifu",
+        uri = "https://api.waifu.pics/sfw/waifu",
         method = "GET",
     })
 
-    return response
+    local image_response = http.make_request({
+        uri = json.parse(response.body).url,
+        method = "GET",
+    })
+
+    image_response.headers["cache-control"] = "no-cache"
+
+    return image_response
 end)
