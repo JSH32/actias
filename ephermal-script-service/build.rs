d1b1d88ebfc2f511828c0befd6e20aecb928cabc
JSH32 <rudnik7000@gmail.com>
2023-01-13 20:20:22 -0600

    Remove extra derives in protobuf buildscript

--- ephermal-script-service/build.rs
+++ ephermal-script-service/build.rs
@@ -1,22 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_client(false)
-        .type_attribute(
-            "script_service.Script",
-            "#[derive(serde::Serialize, serde::Deserialize)]",
-        )
-        .type_attribute(
-            "bundle.Bundle",
-            "#[derive(serde::Serialize, serde::Deserialize)]",
-        )
-        .type_attribute(
-            "bundle.File",
-            "#[derive(serde::Serialize, serde::Deserialize)]",
-        )
-        .type_attribute(
-            "script_service.Revision",
-            "#[derive(serde::Serialize, serde::Deserialize)]",
-        )
         .compile(
             &[
                 "../protobufs/shared/bundle.proto",
