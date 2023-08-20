cd3d654beec7f41cc222e319cc618359434ac45d
JSH32 <rudnik7000@gmail.com>
2023-08-20 10:59:15 -0400

    Remove example

--- actias-cli/helloworld/main.lua
+++
@@ -1,6 +0,0 @@
-add_event_listener("fetch", function(request)
-    return {
-        body = json.stringify({hello = "worsdfsdfld"}),
-        headers = {["Content-Type"] = "application/json"}
-    }
-end)
