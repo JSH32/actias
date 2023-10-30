e3c0c26620013ff80ed58787054cf2f06f7456f9
JSH32 <rudnik7000@gmail.com>
2023-10-30 19:51:17 -0400

    Download page

--- actias-web/src/pages/download.tsx
+++ actias-web/src/pages/download.tsx
@@ -1,26 +1,36 @@
 import { CodeHighlight } from '@mantine/code-highlight';
 import {
-  Button,
   Container,
   Stack,
   Title,
   Text,
   Divider,
   Group,
+  Code,
 } from '@mantine/core';
 import {
   IconBrandUbuntu,
   IconBrandWindows,
   IconDownload,
 } from '@tabler/icons-react';
+import getConfig from 'next/config';
 
 const DownloadPage = () => {
+  const { publicRuntimeConfig } = getConfig();
+
   return (
     <Container size={700} my={40}>
       <Stack gap="lg">
         <Stack align="center">
-          <Title size="h1">Download Actias CLI</Title>
+          <Group>
+            <IconDownload />
+            <Title size="h1">Download Actias CLI</Title>
+          </Group>
           <Text size="md">Have rustup, cargo, and git installed</Text>
+          <Text size="md">
+            <b>API URL</b> for login is{' '}
+            <Code>{publicRuntimeConfig.apiRoot}</Code>
+          </Text>
         </Stack>
         <Divider />
         <Stack align="center">
